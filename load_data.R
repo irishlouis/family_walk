47f98c425befafa1ed4dffc98bb4c08912ff0c80
irishlouis <louissmith@gmail.com>
2016-05-05 10:16:07 +0100

    change to calc of epoch_id SLOW

--- load_data.R
+++ load_data.R
@@ -1,3 +1,4 @@
+# load packages 
 require(data.table)
 require(dplyr)
 require(lubridate)
@@ -27,13 +28,19 @@ for (i in csv.files[str_detect(csv.files, "RAW.csv")]){
   dt[,':='(
     device_id = device_id,
     datetime = dmy_hms(Timestamp),
-    epoch_id = floor_date(dmy_hms(Timestamp), "minute"),
     Timestamp = NULL,
     vec.mag = sqrt(`Accelerometer X`^2 + `Accelerometer Y`^2 + `Accelerometer Z`^2),
     `Accelerometer X` = NULL,
     `Accelerometer Y` = NULL,
     `Accelerometer Z` = NULL
-  ),]
+  ),][,':='(
+      time_minute = floor_date(datetime, "minute"),
+      second = 5 * floor(second(datetime)/5)
+      ),][,':='(
+        epoch_id = time_minute + seconds(second),
+        time_minute = NULL,
+        second = NULL
+      ),]
   
   data[[which(csv.files[str_detect(csv.files, "RAW.csv")] == i)]] <- dt
 }
