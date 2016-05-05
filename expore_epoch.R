7b654e1c51f9de3a6c0ad7ea2339ff5bc0205c9d
irishlouis <louissmith@gmail.com>
2016-05-05 13:41:44 +0100

    changes to plot

--- expore_epoch.R
+++ expore_epoch.R
@@ -1,20 +1,22 @@
-epoch[timestamp >= ymd_hms("20160504 182500") & timestamp <= ymd_hms("20160504 190500") ,.(n_rows = .N,
-         n_steps = sum(steps),
-         mean_steps = mean(steps),
-         max_steps = max(steps),
-         min_steps = min(steps)), serialnumber]
+epoch[timestamp >= ymd_hms("20160504 183000") & timestamp <= ymd_hms("20160504 185500") ,
+      .(n_steps = sum(steps),
+        mean_steps = round(mean(steps),2),
+        sd_steps = round(sd(steps), 2),
+        max_steps = max(steps),
+        min_steps = min(steps)), serialnumber]
 
-ggplot(epoch[timestamp >= ymd_hms("20160504 182500") & timestamp <= ymd_hms("20160504 190500")], 
+ggplot(epoch[timestamp >= ymd_hms("20160504 183000") & timestamp <= ymd_hms("20160504 185500")], 
        aes(timestamp, steps, group = serialnumber)) + 
-  geom_line(aes(col = serialnumber)) +
+  geom_line() +
   facet_wrap(~serialnumber, ncol = 2) + 
-  labs(title = "Summary of step activity from 5s epoch",
-       subtitle = "NOTE: Walk occurred 18:30 to 18:55",
-       caption = "Data filtered to show period between 18:25 - 19:05",
+  labs(title = "Summary of step activity from 5s epochs",
+       subtitle = "Steps per epoch data for adult 2 subjects (1 wearing 2 devices) and a dog.\nContinuous walking except for a short break when one of the subjects went for swim in river.",
+       caption = "Data filtered to show walking period between 18:30 - 18:55",
        y = "Steps / epoch",
-       x = "") + 
+       x = "Time (hh:mm)") + 
   theme_bw() +
-  theme(legend.position = "none")
+  theme(legend.position = "none",
+        panel.grid.minor = element_blank())
 
 # find epochs with all devices similar steps
 dcast(epoch, timestamp ~ serialnumber, fill = NA, value.var = "steps") %>% 
