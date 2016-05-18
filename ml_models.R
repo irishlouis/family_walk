1dc53a3103f0f756dc93e576f13ef1d9a09d1efe
irishlouis <louissmith@gmail.com>
2016-05-18 09:37:53 +0100

    move to single run of list of models

--- ml_models.R
+++ ml_models.R
@@ -1,5 +1,11 @@
-# remove control device
+# remove control device & bin steps
 model.data <- summary[device_id != "TAS1E31150005"][,steps := cut(steps, 5),]
+
+# splits of steps
+table(model.data$steps)
+
+# delete slowest bin
+model.data <- model.data[steps != "(-0.014,2.8]"]
 
 # distribution of steps per device
 table(model.data[,.(steps, device_id),])
@@ -7,12 +13,7 @@ table(model.data[,.(steps, device_id),])
 require(caret)
 require(caretEnsemble)
 
-# partition data
-seeds <- c(456, 465, 783, 315, 97851, 951)
-
-for (seed in seeds) {
-
-set.seed(seed)
+set.seed(456456)
 
 s <- createDataPartition(model.data$device_id, p = 0.6, list = FALSE)
 training <- model.data[s][,':='(
@@ -33,14 +34,14 @@ my_control <- trainControl(
 )
 
 
-set.seed(seed * 3)
 # train list of models using control spec'd above
 model_list <- caretList(device_id ~ ., 
                         data=training, 
                         trControl=my_control,
-                        methodList=c('xgbTree', 'rf', 'svmRadial'))
+                        methodList=c('gbm','xgbTree', 'rf', 'svmRadial'))
 
-print(lapply(model_list, 
-       function(x) confusionMatrix(predict(x, testing), testing$device_id)))
-}
+# look at validation results for each 
+lapply(model_list, 
+       function(x) confusionMatrix(predict(x, testing), testing$device_id))
 
+# encouraging results
